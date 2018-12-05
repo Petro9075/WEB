6a24517f6b3e392a89518c9421969252fce00d2f
Petro9075 <45629253+Petro9075@users.noreply.github.com>
2018-12-05 17:18:43 +0200

    Add files via upload

---
+++ Laba3/laba3.js
@@ -0,0 +1,7 @@
+
+    var head = $("head").html();
+    alert(head);
+    $("#list-header").css("font-weight", "bold");
+    $("#list-header").text("11");
+    $("ul li:nth-child(2)").text("Petro");
+    $("li").css("color","#555");
